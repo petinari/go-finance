68dd75dc175666b9980ad92f269fa5b7acd46864
Robson <petinari@outlook.com>
2023-03-10 16:01:35 -0400

    3

---
+++ db/query/category.sql
@@ -0,0 +1,16 @@
+--name: CreateCategory :one
+insert into categories (user_id, title, type, description)
+values ($1,$2, $3,$4) returning *;
+
+--name: GetCategory :one
+select *
+from categories where id = $1 limit 1;
+
+--name: GetCategories :many
+select * from categories where user_id = $1 and type = $2 and title like $3 and description like $4;
+
+--name: UpdateCategory :one
+update categories set title = $1, description = $2 where id = $3 returning *;
+
+--name: DeleteCategory :exec
+delete from categories where id = $1;
